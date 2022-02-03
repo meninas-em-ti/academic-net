1c2775f188beba487cdba1b94dbc0cc2e61f256a
Joyce <joyce.apsmartins@gmail.com>
2022-02-03 22:34:28 +0000

    Setting up

---
+++ src/main/java/com/java/study/group/academicnet/AcademicNetApplication.java
@@ -0,0 +1,14 @@
+package com.java.study.group.academicnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class AcademicNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/study/group/academicnet/AcademicNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.study.group.academicnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
